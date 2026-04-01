84c755ba772e5cba413621975397ab7a6a756dc1
rD227 <2329976818@qq.com>
2026-04-01 22:45:21 +0800

    Fixing the crash when start the service

--- primitiveFTPd/src/org/primftpd/ui/LeanbackActivity.java
+++ primitiveFTPd/src/org/primftpd/ui/LeanbackActivity.java
@@ -3,12 +3,18 @@ package org.primftpd.ui;
 
 import android.view.Menu;
 
+import androidx.annotation.NonNull;
+
 public class LeanbackActivity extends MainTabsActivity {
 
+    @NonNull
     @Override
     protected PftpdFragment createPftpdFragment() {
         return new LeanbackFragment();
     }
+    //Original version: 未注解的方法重写注解为 @NotNull 的方法
+    //我猜改了也没事（？
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
